751a5788b5fd9202ef4ed3e65cde23556bedcc5d
Glauber Santiago <glaubersantiago@outlook.com>
2025-01-29 21:16:10 -0300

    1 - map-filter

---
+++ map-filter.js
@@ -0,0 +1,7 @@
+// 1 - Crie um array de números e utilize o método map para retornar um novo array com todos os números dobrados.
+
+const numbers = [1, 2, 3, 4, 5];
+
+let resultado = numbers.map(number => number * 2)
+
+console.log(resultado)
